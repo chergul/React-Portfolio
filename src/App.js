94b3fe5a871eef8d514953195cbf371c11b1f2f1
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-07-31 18:18:25 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import MyNavbar from './components/MyNavbar';
 
 function App() {
   return (
-
-    <div className="App">
+  <Router>
+        <div className="App">
       <MyNavbar />
-      {/* <About />
-      <Portfolio />
-      <Contact /> */}
+      {/* <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/project" component={Portfolio} />
+          <Route path="/project" component={Contact} />
+          
+      </Switch> */}
       <Footer />
     </div>
+  </Router>
+
 
 
 
