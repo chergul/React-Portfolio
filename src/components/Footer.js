bc3f693676b2994fdecea7970729239b97e37f90
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-07-31 13:24:57 -0400

    some npm pacnpm packages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,19 +12,9 @@ import {
     let date = new Date();
     let year = date.getFullYear();
     return ( 
-<div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    {/* <h5 class="card-title">Special title treatment</h5> */}
-    <p class="card-text">Designed and Developed by Ceyda H. Hergul</p>
-    <h2>Copyright © {year} CHH</h2>
-  </div>
-  <div class="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
+    <div>
+      test my footer
+    </div>
     );
   }
   
