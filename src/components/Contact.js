062775ab5787bf5b1a25b10e9f4b39ca6bcb90a8
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-08-03 00:07:32 -0400

    contact page basic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,23 @@ import '../style.css';
 function Contact () {
     return (
         <div>
-            Test for CONTACT
+            <h1>Get in touch with me</h1>
+        <div className="container">
+            <div className="git-cont row">
+                <div className="col-12 col-sm-6 half">
+                    <form 
+                    // action={contact.contactUrl ? contact.contactUrl : "https://formspree.io"} method={contact.contactUrl ? "POST" : "GET"}
+                    >
+                        <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
+                        <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
+                        <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
+                        <textarea id="msg" name="message" placeholder="Message" required></textarea>
+                        <button style={{cursor: 'pointer'}} type="submit">Send Message</button>
+                    </form>
+                </div>
+            </div>
         </div>
+    </div>
 
         
     )
