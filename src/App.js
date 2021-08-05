7677adccab312005f905f6f6c298155c70cd8283
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-08-05 15:02:50 -0400

    adding redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import MyNavbar from './components/MyNavbar';
 import Portfolio from './components/Portfolio';
@@ -13,12 +13,13 @@ function App() {
   <Router>
         <div className="App">
       <MyNavbar />
-      
+
       <Switch>     
         <Route path="/about" component={About} />
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
-        <Route path="*" exact component={Home} />
+        <Route path="/" exact component={Home} />
+        <Redirect to="/"/>
       </Switch>
 
       <Footer />
